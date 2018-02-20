3c926215c6371d5231a05be078ec148f55059699
EvgeneOskin <eoskin@crystalnix.com>
2018-02-20 22:45:16 +0600

    WIP Make for easy to enable deep sleep

--- src/main.ino
+++ src/main.ino
@@ -4,20 +4,24 @@
 #include <WiFiUDP.h>
 #include "DHT.h"
 
+extern "C" {
+  #include "user_interface.h"
+}
 const char* ssid = "*******";
 const char* password = "*******";
 const char* statsdsIP = "*******";
 const int statsdsPort = 8125;
 
 const int STATSD_PACKET_SIZE = 48;
-byte statsdPacketBuffer[STATSD_PACKET_SIZE];
+char statsdPacketBuffer[STATSD_PACKET_SIZE];
 struct {
-  const char* temperature = "temperature";
-  const char* humidity = "humidity";
+  const char* temperature = "office.sensor.2.temperature";
+  const char* humidity = "office.sensor.2.humidity";
 } statsOptions;
 
 #define DHTPIN SCL
 #define DHTTYPE DHT22
+#define DELAY_SECONDS_PERIOD 60
 
 const int led = 13;
 
@@ -28,30 +32,52 @@ void setup(void){
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
-  Serial.println("Before connect to wifi");
+
+  Serial.println("Thermometer started");
+  dht.begin();
+}
+
+void loop(void) {
+  float humidity;
+  float temperatureCelsius;
+
+  sleep();
+  humidity = dht.readHumidity();
+  temperatureCelsius = dht.readTemperature();
+
+  wake();
+  sendStatsdData(statsOptions.temperature, temperatureCelsius);
+  sendStatsdData(statsOptions.humidity, humidity);
+}
+
+void sleep() {
+  delay(DELAY_SECONDS_PERIOD*1000);
+}
+
+void wake() {
+  connectWiFi();
+}
+
+void connectWiFi() {
   WiFi.begin(ssid, password);
   Serial.println("Connecting to wifi");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  int connRes = WiFi.waitForConnectResult();
+  Serial.print("WiFi connRes: ");
+  Serial.println(connRes);
+  if (connRes != WL_CONNECTED) {
+    Serial.println ( "WiFi not connected" );
+  } else {
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("Termometer started");
-  dht.begin();
 }
 
 void sendStatsdData(const char* key, float value) {
   if (isnan(value)) {
-    Serial.print("can not write ");
-    Serial.print(key);
-    Serial.println("");
+    Serial.print("will not send empty data");
+    Serial.println(key);
     return;
   }
   memset(statsdPacketBuffer, 0, STATSD_PACKET_SIZE);
@@ -61,24 +87,15 @@ void sendStatsdData(const char* key, float value) {
   /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
   dtostrf(value, 4, 2, str_value);
 
-  Serial.println(str_value);
   sprintf((char*)statsdPacketBuffer, "%s:%s|g\n", key, str_value);
 
-  udp.beginPacket(statsdsIP, statsdsPort);
-  udp.write(statsdPacketBuffer, STATSD_PACKET_SIZE);
-  udp.endPacket();
-}
+  int error = udp.beginPacket(statsdsIP, statsdsPort);
+  Serial.print("begin packet ");
+  Serial.println(error); // 1 for OK
 
-void loop(void){
-  float humidity;
-  float temperatureCelsius;
+  udp.write(statsdPacketBuffer);
 
-  while(true) {
-    delay(500);
-    Serial.println("working...");
-    humidity = dht.readHumidity();
-    temperatureCelsius = dht.readTemperature();
-    sendStatsdData(statsOptions.temperature, temperatureCelsius);
-    sendStatsdData(statsOptions.humidity, humidity);
-  }
+  error = udp.endPacket(); // 1 for OK
+  Serial.print("end packet ");
+  Serial.println(error);
 }
